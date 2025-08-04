52d34ec6f01853467a7006b153a298358ba991e3
megha24 <agarwal.megha7@gmail.com>
2025-08-04 10:54:11 +0530

    fixed upload image

--- src/main/java/com/mb/ocrservice/controller/DocumentController.java
+++ src/main/java/com/mb/ocrservice/controller/DocumentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/documents")
+@RequestMapping("api/documents")
 @Slf4j
 public class DocumentController {
 
